544c6576a0b563af44c10cc634b57862bb2dadf8
NyakoFox <nyakowofox@gmail.com>
2022-09-28 22:52:05 -0300

    accuracy for unused virovirokun ACTs

--- libraries/Virovirokun/scripts/battle/cutscenes/virovirokun.lua
+++ libraries/Virovirokun/scripts/battle/cutscenes/virovirokun.lua
@@ -4,16 +4,24 @@ return {
             battler:heal(30)
         end
         cutscene:text("* Ralsei cooked up a cure.")
-        cutscene:text("* If you're sick,[wait:5] shouldn't\nyou have some soup?[wait:5]\nSay \"aah\"~!", "blush_pleased", "ralsei")
-        cutscene:text("* Sickness was cured![wait:5] Everyone's\nHP up!")
+        cutscene:text("* If you're sick,\nshouldn't you have some\nsoup? Say \"aah\"~!", "blush_pleased", "ralsei")
+        cutscene:text("* Sickness was cured! Everyone's\nHP up!")
     end,
 
     cook_susie = function(cutscene, battler, enemy)
         cutscene:text("* Susie cooked up a cure!")
-        cutscene:text("* What,[wait:5] you want me to cook\nsomething?", "smile", "susie")
+        cutscene:text("* What, you want me to\ncook something!?", "smile", "susie")
         cutscene:text("* Susie put a hot dog in the\nmicrowave!")
-        enemy:explode(0, 0, true)
+        if false then
+            local susie = cutscene:getCharacter("susie")
+            local explosion = susie:explode(0, 0, true)
+            explosion:setScale(1)
+            explosion.speed = 1
+            explosion:setOrigin(0.5, 0.75)
+        else
+            enemy:explode(0, 0, true)
+        end
         enemy:hurt(enemy.health * 0.75, battler)
-        cutscene:text("* She forgot to poke holes in it![wait:5]\nThe hot dog exploded!")
+        cutscene:text("* She forgot to poke holes in it!\nThe hot dog exploded!")
     end
 }
