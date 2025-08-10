0b2eeaacbb1fcc4ad25a2a9215b01d33a03790f8
JustAnotherRandomGithubUser <125609186+JustAnotherRandomGithubUser@users.noreply.github.com>
2025-08-10 13:26:24 -0400

    Polished up aiming diamonds attack to be a little more accurate.*
    *For some reason, the spawning positions of the bullet in the wave seem to be a bit off compared to how they are in the original game, despite the code for the wave being set up in the same way as DR's.
    Currently unsure on how to fix that atm.

--- libraries/Rudinn/scripts/battle/waves/rudinn/diamonds_aim.lua
+++ libraries/Rudinn/scripts/battle/waves/rudinn/diamonds_aim.lua
@@ -1,27 +1,37 @@
 local DiamondsAim, super = Class(Wave)
 
 function DiamondsAim:init()
-	super:init(self)
+	super.init(self)
+
+    self.time = 140/30
 end
 
 function DiamondsAim:onStart()
-    self.timer:every(2/5, function()
-        local x = Utils.random(Game.battle.arena.left - 100, Game.battle.arena.right + 100)
-        local y = Utils.random(Game.battle.arena.top - 20, Game.battle.arena.top - 70)
+    local ratio = self:getEnemyRatio()
 
-        local bullet = self:spawnBullet("rudinn/diamond_black", x, y)
-		self.timer:script(function(wait)
-			wait(1/3)
-			bullet.targeting = false
-			bullet.graphics.grow = 0
-			wait(1/3)
-			local bullet2 = self:spawnBullet("rudinn/diamond_white", bullet.x, bullet.y, bullet.rotation)
-			bullet2:setLayer(self.layer + 0.01)
-			wait(1/4)
-			bullet.graphics.grow = 0.2
-			bullet:fadeOutAndRemove(0.2)
-		end)
+    self.timer:every(12/30*ratio, function()
+        for _, attacker in ipairs(self:getAttackers()) do
+            local soul = Game.battle.soul
+			
+            local dir = 30 + love.math.random(120)
+            local radius = 140 + love.math.random(80)
+            local x = math.cos(dir)*radius
+            local y = math.sin(dir)*radius
+		
+            local diamond = self:spawnBullet("rudinn/diamond_black", soul.x + x, soul.y + y)
+        end
     end)
+end
+
+function DiamondsAim:getEnemyRatio()
+    local enemies = #Game.battle:getActiveEnemies()
+    if enemies <= 1 then
+        return 1
+    elseif enemies == 2 then
+        return 1.6
+    elseif enemies >= 3 then
+        return 2.3
+    end
 end
 
 return DiamondsAim
