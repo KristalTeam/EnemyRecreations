884ad3bbf500e9cc2b39e6c1f433fba239eebe1f
JustAnotherRandomGithubUser <125609186+JustAnotherRandomGithubUser@users.noreply.github.com>
2025-08-09 22:08:32 -0400

    Made vertical diamonds attack more accurate.

--- libraries/Rudinn/scripts/battle/waves/rudinn/diamonds_up_lots.lua
+++ libraries/Rudinn/scripts/battle/waves/rudinn/diamonds_up_lots.lua
@@ -1,18 +1,37 @@
 local DiamondsUpLots, super = Class(Wave)
 
 function DiamondsUpLots:init()
-	super:init(self)
+	super.init(self)
 	
-	self.time = 7
+    self.time = 140/30
 end
 
 function DiamondsUpLots:onStart()
-    self.timer:every(1/15, function()
-        local x = Utils.random(Game.battle.arena.left - 20, Game.battle.arena.right + 20)
-        local y = Utils.random(Game.battle.arena.bottom + 50, Game.battle.arena.bottom + 100)
+    local ratio = self:getEnemyRatio()
 
-        self:spawnBullet("rudinn/diamond_mix", x, y, math.rad(270))
+    self.timer:every(1/15*ratio, function()
+        for _, attacker in ipairs(self:getAttackers()) do
+            local x = -100 + love.math.random(200)
+            local y = 140 + love.math.random(40)
+            local num = Utils.pick{0, 1, 2, 3}
+            if (num == 3) then
+                x = -10 + love.math.random(20)
+            end
+
+            self:spawnBullet("rudinn/diamond_mix", Game.battle.soul.x + x, Game.battle.soul.y + y, math.rad(270))
+        end
     end)
+end
+
+function DiamondsUpLots:getEnemyRatio()
+    local enemies = #Game.battle:getActiveEnemies()
+    if enemies <= 1 then
+        return 1
+    elseif enemies == 2 then
+        return 1.6
+    elseif enemies >= 3 then
+        return 2.3
+    end
 end
 
 return DiamondsUpLots
